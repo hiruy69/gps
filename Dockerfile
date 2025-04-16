347c0e00a0e282c7cd830d47c48ddc2c3e0d0359
hiruy69 <hiruytamiru@gmail.com>
2025-04-16 13:50:02 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Use a lightweight Python image
+FROM python:3.10-slim
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the script
+COPY rabbit_mq_publisher.py .
+COPY gps.py .
+
+# Expose the TCP listening port
+EXPOSE 5151
+
+# Run the listener script
+CMD ["python", "gps.py"]
---
+++ rabbit_mq_publisher.py
@@ -0,0 +1,59 @@
+import pika
+import os
+from dotenv import load_dotenv
+import json
+import uuid
+# Load environment variables from .env file
+load_dotenv()
+
+class RabbitMQClient:
+    def __init__(self):
+        self.connection = None
+        self.channel = None
+        self.queue_name = os.getenv('RABBITMQ_QUEUE', 'my_queue')
+        self.routing_key = os.getenv('RABBITMQ_ROUTING_KEY', 'gps_data')
+        self.exchange = os.getenv('RABBITMQ_EXCHANGE', 'gps_exchange')
+        self._connect()
+
+    def _connect(self):
+        """Establish and store the RabbitMQ connection and channel."""
+        if not self.connection or self.connection.is_closed:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host=os.getenv("RABBITMQ_HOST", "localhost"),
+                    port=int(os.getenv("RABBITMQ_PORT", 5672)),
+                    credentials=pika.PlainCredentials(
+                        os.getenv("RABBITMQ_USER", "guest"),
+                        os.getenv("RABBITMQ_PASSWORD", "guest")
+                    )
+                )
+            )
+        self.channel = self.connection.channel()
+
+    def send(self, data: dict,queue_name: str = None):
+        """Send a JSON-encoded dictionary to the specified queue."""
+        if not self.channel or self.channel.is_closed:
+            self._connect()
+        queue_name = queue_name or self.queue_name
+        self.channel.queue_declare(queue=queue_name, durable=True)
+
+        message_id = str(uuid.uuid4())
+        body = json.dumps(data)
+
+        self.channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=queue_name,
+            body=body,
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # Make message persistent
+                content_type='application/json',
+                message_id=message_id,
+            )
+        )
+
+        print(f"[x] Sent to {queue_name} | ID: {message_id}")
+
+    def close(self):
+        """Manually close the connection if needed."""
+        if self.connection and not self.connection.is_closed:
+            self.connection.close()
